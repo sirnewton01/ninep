458174bd7a6820a139e16b1d36c4fdf3e2f54b07
Ronald G. Minnich <rminnich@gmail.com>
2016-06-02 14:43:44 -0700

    Add ufs in a new location
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ cmd/ufs/ufs.go
@@ -0,0 +1,41 @@
+// This is a ufs server.
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+
+	"github.com/rminnich/ninep/stub"
+	"github.com/rminnich/ninep/filesystem"
+)
+
+var (
+	ntype = flag.String("ntype", "tcp4", "Default network type")
+	naddr = flag.String("addr", ":5640", "Network address")
+)
+
+func main() {
+	flag.Parse()
+	l, err := net.Listen(*ntype, *naddr)
+	if err != nil {
+		log.Fatalf("Listen failed: %v", err)
+	}
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Printf("Accept: %v", err)
+		}
+
+		_, err = ufs.NewUFS(func(s *stub.Server) error {
+			s.FromNet, s.ToNet = c, c
+			s.Trace = log.Printf
+			return nil
+		})
+		if err != nil {
+		   log.Printf("Error: %v", err)
+		}
+	}
+
+}
